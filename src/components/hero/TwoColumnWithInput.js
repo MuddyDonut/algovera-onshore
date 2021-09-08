99b5f62619463edc3f7eb654900570219d546a9d
Muddy Donut <julian1559@gmail.com>
2021-09-08 16:39:30 -0600

    added email back functionality -JM

--- src/components/hero/TwoColumnWithInput.js
+++ src/components/hero/TwoColumnWithInput.js
@@ -21,12 +21,9 @@ const Heading = tw.h1`font-bold text-3xl md:text-3xl lg:text-4xl xl:text-5xl tex
 const Paragraph = tw.p`my-5 lg:my-8 text-base xl:text-lg`;
 
 const Actions = styled.div`
-  ${tw`relative max-w-md text-center mx-auto lg:mx-0`}
-  input {
-    ${tw`sm:pr-48 pl-8 py-4 sm:py-5 rounded-full border-2 w-full font-medium focus:outline-none transition duration-300  focus:border-primary-500 hover:border-gray-500`}
-  }
+  ${tw`relative max-w-md mx-auto lg:mx-0`}
   button {
-    ${tw`w-full sm:absolute right-0 top-0 bottom-0 bg-primary-500 text-gray-100 font-bold mr-2 my-4 sm:my-2 rounded-full py-4 flex items-center justify-center sm:w-40 sm:leading-none focus:outline-none hover:bg-primary-900 transition duration-300`}
+    ${tw`w-full md:right-0 bg-primary-500 text-gray-100 font-bold mr-2 my-4 sm:my-2 rounded-full py-4 items-center justify-center sm:w-40 sm:leading-none focus:outline-none hover:bg-primary-900 transition duration-300`}
   }
 `;
 
@@ -63,9 +60,9 @@ export default ({ roundedHeaderButton }) => {
             </Paragraph>
             <Actions>
               
-              <input name="email" type="email" placeholder="Your E-mail Address" />
-              <button type="submit">Get on the List</button>
-
+              {/* <input name="email" type="email" placeholder="Your E-mail Address" /> */}
+              <a href="https://share.hsforms.com/1YXZ6_OwXRgOuKexSsGdDVQ55vc7"><button type="submit">Get on the List</button></a> 
+<br></br>
             </Actions>
             <CustomersLogoStrip>
               <p>Our TRUSTED Supporters</p>
