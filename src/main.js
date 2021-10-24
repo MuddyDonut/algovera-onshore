ae6c3dff35751936575fce96f5afe3ff55e98170
Muddy Donut <julian1559@gmail.com>
2021-10-24 10:55:41 -0600

    added procfile - JM

--- src/main.js
+++ src/main.js
@@ -7,7 +7,3 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
-
-App.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, App.settings.env);
-});
